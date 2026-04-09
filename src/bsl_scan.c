393ab1240a76a3af49ba02dc532d4b25f41d6001
ili1116 <266364838+illliliiill1116@users.noreply.github.com>
2026-04-09 11:31:42 +0800

    init

--- src/bsl_scan.c
+++ src/bsl_scan.c
@@ -304,9 +304,138 @@ top_retry:;
     }
 }
 
+static inline void 
+_bsl_scan_n_batch_optimistic(bsl_t *list, bsl_key_t start, size_t length, range_batch_cb cb, void *arg)
+{
+    if (unlikely(length <= 0)) return;
+
+    size_t remaining = length;
+    bsl_key_t current_start = start;
+
+top_retry:;
+
+    node_header_t *curr = list->headers[MAX_LEVEL - 1];
+    hocc64_t curr_v = NODE_LOAD_VERSION(curr);
+
+    for (int level = MAX_LEVEL - 1; level > 0; level--)
+    {
+        while (LOAD_RELAXED(curr->next_header) <= current_start)
+        {
+            node_header_t *next = LOAD_RELAXED(curr->next);
+            if (!next) break;
+
+            hocc64_t next_v = NODE_LOAD_VERSION(next);
+
+            if (!NODE_VALIDATE(curr, curr_v))
+                goto top_retry;
+
+            curr = next;
+            curr_v = next_v;
+        }
+
+        int rank = find_rank(NODE_KEYS(curr), LOAD_RELAXED(curr->num_elts), current_start);
+        node_header_t *child = LOAD_RELAXED(INTERNAL_CHILDREN(curr)[rank]);
+        if (!child) goto top_retry; 
+
+        /* two-phase HOH validation, see bsl_get.c */
+        if (curr_v & HOCC_WRITER_BIT || !NODE_VALIDATE(curr, curr_v))
+            goto top_retry;
+        
+        hocc64_t child_v = NODE_LOAD_VERSION(child);
+
+        if (!NODE_VALIDATE(curr, curr_v))
+            goto top_retry;
+
+        curr = child;
+        curr_v = child_v;
+    }
+    
+    while (LOAD_RELAXED(curr->next_header) <= current_start)
+    {
+        node_header_t *next = LOAD_RELAXED(curr->next);
+        if (!next) break;
+
+        hocc64_t next_v = NODE_LOAD_VERSION(next);
+
+        if (!NODE_VALIDATE(curr, curr_v))
+            goto top_retry;
+
+        curr = next;
+        curr_v = next_v;
+    }
+
+    leaf_node_t *leaf = (leaf_node_t *)curr;
+
+    int num_elts = LOAD_RELAXED(leaf->header.num_elts);
+    int rank = find_rank(leaf->keys, num_elts, current_start);
+
+    if (LOAD_RELAXED(leaf->keys[rank]) != current_start || (current_start == BSL_KEY_MIN && leaf == list->headers[0]))
+    {
+        rank++;
+        if (rank == num_elts)
+        {
+            leaf_node_t *next = (leaf_node_t *)LOAD_RELAXED(leaf->header.next);
+            if (leaf->header.next_header == BSL_KEY_MAX)
+            {
+                return;
+            }
+            
+            if (!next) goto top_retry;
+
+            hocc64_t next_v = NODE_LOAD_VERSION(next);
+
+            if (!NODE_VALIDATE(curr, curr_v))
+                goto top_retry;
+
+            rank = 0;
+            leaf = next;
+            curr_v = next_v;
+        }
+    }
+
+    while (remaining)
+    {
+        int num = LOAD_RELAXED(leaf->header.num_elts);
+
+        size_t batch_size = num - rank;
+        if (remaining < batch_size)
+            batch_size = remaining;
+
+        if (batch_size > 0)
+        {
+            bsl_range_t range = {
+                .keys = &leaf->keys[rank],
+                .vals = &leaf->values[rank],
+                .count = batch_size
+            };
+            
+            cb(range, arg);
+            remaining -= batch_size;
+            
+            current_start = LOAD_RELAXED(leaf->keys[rank + batch_size - 1]) + 1;
+        }
+
+        leaf_node_t *next = (leaf_node_t *)LOAD_ACQUIRE(leaf->header.next);
+        if (leaf->header.next_header == BSL_KEY_MAX || remaining == 0)
+        {
+            break;
+        }
+        if (!next) goto top_retry;
+
+        hocc64_t next_v = NODE_LOAD_VERSION(leaf);
+
+        if (!NODE_VALIDATE(curr, curr_v))
+            goto top_retry;
+
+        rank = 0;
+        leaf = next;
+        curr_v = next_v;
+    }
+}
+
 void bsl_scan_n_batch(bsl_t *list, bsl_key_t start, size_t length, range_batch_cb cb, void *arg)
 {
     epoch_enter();
-    _bsl_scan_n_batch(list, start, length, cb, arg);
+    _bsl_scan_n_batch_optimistic(list, start, length, cb, arg);
     epoch_exit();
 }
